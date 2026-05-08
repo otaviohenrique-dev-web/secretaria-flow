4c6a9090a86424d04e5a01125a4b96201b7349d6
resolutejax <otavio-henrique10@hotmail.com>
2026-05-08 20:28:17 -0300

    ⚙️Fix: Atualizando o "CORSMiddleware" para o endereço Vercel

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Secretaria Flow API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://secretaria-flow.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
